1dcea20d827b6b3d080151b3637d6684986ec4a2
ShyamGanesh1307 <shyamganesh.v2025@vitstudent.ac.in>
2026-01-11 11:09:44 +0530

    Create linear_regression_updated.py

---
+++ linear_regression_updated.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = {
+    'Hours_Studied': [1, 2, 3, 4, 5],
+    'Marks': [40, 50, 60, 70, 80]
+}
+
+df = pd.DataFrame(data)
+X = df[['Hours_Studied']]
+y = df['Marks']
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Mean Squared Error:", mse)
+print("R2 Score:", r2)
+
+plt.scatter(X, y, color='blue', label='Actual Data')
+plt.plot(X, model.predict(X), color='red', label='Regression Line')
+plt.xlabel('Hours Studied')
+plt.ylabel('Marks')
+plt.title('Linear Regression Model')
+plt.legend()
+plt.show()
